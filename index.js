3413eedabe183631ba02dad9adfd67c284788aaf
birhanu <birhanutbelay@gmail.com>
2025-04-29 10:38:31 +0300

    Code Added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Create a function constructor called Employee that accepts a name, position, and salary. Create an array of 5 employees. Write a program that increases the salary by 10% for employees who have the position "developer" and print the updated employee list. Create a function constructor called Employee that accepts a name, position, and salary. Create an array of 5 employees. Write a program that increases the salary by 10% for employees who have the position "developer" and print the updated employee list.
+//first question
 function Employee(name,position,salary){
     this.name=name;
     this.position=position;
@@ -21,7 +21,7 @@ function increaseSalary(employeesData){
 };
 increaseSalary(employeesData);
 
-// Given an array of product objects (each with name, price, and inStock properties), write a function that returns a new array containing only the products that are inStock: true, and sort the available products by price in ascending order. 
+//second question
 const productsObject=[
     {name:"Refrigerator",price:55000,inStock:false},
     {name:"Laptop",price:200000,inStock:true},
@@ -37,7 +37,7 @@ const productsObject=[
  };
  console.log(filterProducts(productsObject));
 
-//  Create an object called grades where the keys are student names and the values are arrays of their scores. Write a function that calculates and prints each student's average score, and if the average is above 70, print "Pass"; otherwise, print "Fail" next to their name.
+//third question
 const grades={
     Wunesh:[56,78,90,56,78],
     Zufan:[98,90,78,56,45],
@@ -60,7 +60,7 @@ function evaluateStudents(grades){
 };
 evaluateStudents(grades);
 
-// Write a function constructor called User that takes username, email, and isActive (boolean). Create an array of users. Write a program that loops through the array and deactivates users who have not logged in recently (simulate this with a random isActive: false assignment) and print out the usernames of active users. 
+//forth question
 function User(username,email,isActive){
     this.username=username;
     this.email=email;
@@ -86,6 +86,8 @@ function deactivateUsers(arrayOfUsers){
     console.log(activeUsers);
 };
 deactivateUsers(arrayOfUsers);
+
+//fifth question
 const destinations = [
     { name: "Singapore", distance: 9000, budgetRequired: 10000},
     { name: "Israel", distance: 7800, budgetRequired: 45000 },
@@ -93,19 +95,17 @@ const destinations = [
     { name: "Russia", distance: 12000, budgetRequired: 78500 },
     { name: "France", distance: 10000, budgetRequired: 18000}
 ];
-function findDestinations(maxDistance, budget) {
+function searchDestinations(maxDistance, budget) {
     const availableDestinations = destinations.filter(destination =>
         destination.distance <= maxDistance && destination.budgetRequired <= budget
     );
     if (availableDestinations.length > 0){
-       return  availableDestinations
+       return availableDestinations
     }
     else{
-      console.log("No destinations available under your budget and distance")
+      return "No destinations available under your budget and distance"
     }
    
 }
-const affordableDestinations = findDestinations(1000, 2000);
-console.log(affordableDestinations);
-
-
+const affordableDestinations = searchDestinations(1000, 2000);
+console.log(searchDestinations(affordableDestinations));
